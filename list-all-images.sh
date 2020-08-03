1204471205b0ddcc5eb4f8903930116dc7c5f5c0
Alex K. Austin <akac92@gmail.com>
2020-08-02 22:13:03 -0700

    Update list-all-images.sh
    Remove clutter comments from original author

--- list-all-images.sh
+++ list-all-images.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
-## @copyright 2018 DennyZhang.com
-## Licensed under MIT
-## https://www.dennyzhang.com/wp-content/mit_license.txt
-##
-## Author : Denny <https://www.dennyzhang.com/contact>
-## Description :
-## https://cheatsheet.dennyzhang.com/cheatsheet-kubernetes-A4
-## --
-## Created : <2018-07-17>
-## Updated: Time-stamp: <2018-12-30 19:07:42>
-##-------------------------------------------------------------------
-# https://kubernetes.io/docs/tasks/access-application-cluster/list-all-running-container-images/
+# Stolen from https://kubernetes.io/docs/tasks/access-application-cluster/list-all-running-container-images/
+# The command below will recursively return all fields named image for all items returned.
 kubectl get pods --all-namespaces -o jsonpath="{..image}" |\
 tr -s '[[:space:]]' '\n' |\
 sort |\
 uniq -c
+
+#------------------------------------------------------------------------------------------------------------------
+# As an alternative, it is possible to use the absolute path to the image field within the Pod. 
+# This ensures the correct field is retrieved even when the field name is repeated, e.g. many fields are called 
+# name within a given item:
+
+#kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec.containers[*].image}"
+#The jsonpath is interpreted as follows:
+
+# .items[*]: for each returned value
+# .spec: get the spec
+# .containers[*]: for each container
+# .image: get the image
